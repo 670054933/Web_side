27c97f12bf2e416d8b8cad920d836e4730c59482
zxx <670054933@qq.com>
2021-12-01 16:16:13 +0800

    接口

---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+// export default{
+//     title:'Teachers_management',
+//
+//     requestConfig:{
+//         platform:{
+//             dev:'',
+//             prod:'',
+//             isEncrypt: false,
+//         }
+//     }
+// }
---
+++ src/utils/request.js
@@ -0,0 +1,116 @@
+import axios from 'axios';
+import QS from 'qs';
+import store from '@/store/index'
+import { Message } from 'element-ui';
+
+
+const service = axios.create({
+    baseURL: process.env.NODE_ENV === 'http://45k1b40509.zicp.vip/',
+    headers:{
+        post:{
+            'Content-Type': 'application/json;charset=utf-8'
+        }
+    },
+    withCredentials: true,
+    timeout: 30000,
+    validateStatus: function () {
+        // 使用async-await，处理reject情况较为繁琐，所以全部返回resolve，在业务代码中处理异常
+        return true
+    },
+    transformRequest: [function (data) {
+        data = JSON.stringify(data)
+        return data
+    }],
+    // 在传递给 then/catch 前，修改响应数据
+    transformResponse: [function (data) {
+        if (typeof data === 'string' && data.startsWith('{')) {
+            data = JSON.parse(data)
+        }
+        return data
+    }]
+
+})
+
+service.interceptors.request.use((config) => {
+    return config
+}, (error) => {
+    // 错误抛到业务代码
+    error.data = {}
+    error.data.msg = '服务器异常，请联系管理员！'
+    return Promise.resolve(error)
+})
+
+const showStatus = (status) => {
+    let message = ''
+    // 这一坨代码可以使用策略模式进行优化
+    switch (status) {
+        case 400:
+            message = '请求错误(400)'
+            break
+        case 401:
+            message = '未授权，请重新登录(401)'
+            break
+        case 403:
+            message = '拒绝访问(403)'
+            break
+        case 404:
+            message = '请求出错(404)'
+            break
+        case 408:
+            message = '请求超时(408)'
+            break
+        case 500:
+            message = '服务器错误(500)'
+            break
+        case 501:
+            message = '服务未实现(501)'
+            break
+        case 502:
+            message = '网络错误(502)'
+            break
+        case 503:
+            message = '服务不可用(503)'
+            break
+        case 504:
+            message = '网络超时(504)'
+            break
+        case 505:
+            message = 'HTTP版本不受支持(505)'
+            break
+        default:
+            message = `连接出错(${status})!`
+    }
+    return `${message}，请检查网络或联系管理员！`
+}
+// 响应拦截器
+service.interceptors.response.use((response) => {
+    const status = response.status
+    let msg = ''
+    if (status < 200 || status >= 300) {
+        // 处理http错误，抛到业务代码
+        msg = showStatus(status)
+        if (typeof response.data === 'string') {
+            response.data = { msg }
+        } else {
+            response.data.msg = msg
+        }
+    }
+    return response
+}, (error) => {
+    // 错误抛到业务代码
+    error.data = {}
+    error.data.msg = '请求超时或服务器异常，请检查网络或联系管理员！'
+    return Promise.resolve(error)
+})
+
+export function post(url, params) {
+    return new Promise((resolve, reject) => {
+        axios.post(url, QS.stringify(params))
+            .then(res => {
+                resolve(res.data);
+            })
+            .catch(err =>{
+                reject(err.data)
+            })
+    });
+}
