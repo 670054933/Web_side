4caf223a940f5678e05def588d7725c552e5ef2b
QX0920 <92423672+QX0920@users.noreply.github.com>
2021-12-10 15:44:36 +0800

    add the title and redesign the form

--- src/views/Information.vue
+++ src/views/Information.vue
@@ -4,21 +4,31 @@
     <div class="right">
       <Header></Header>
       <div class="main">
-        <div>
-          内容写在这里
+        <div class="title">
+          <img src="@/image/头像.png" alt=""/>
+          <p class="char">Name: {{ name }}</p>
+          <p class="char">Post: {{ post }}</p>
+          <p class="char">Class taught: {{ classtaught }}</p>
         </div>
         <!--  表单  -->
         <el-form ref="form" :model="form" label-width="120px">
-          <el-form-item label="Phone number:">
-            <el-input v-model="form.phoneNO"></el-input>
-          </el-form-item>
-          <el-form-item label="E-mail:">
-            <el-input v-model="form.email"></el-input>
-          </el-form-item>
-          <el-form-item label="Subjects taught:">
+          <el-row>
+            <el-col :span="11">
+              <el-form-item label="Phone number:"  label-width="180px">
+                <el-input v-model="form.phoneNO"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="11">
+              <el-form-item label="E-mail:"  label-width="180px">
+                <el-input v-model="form.email"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+
+          <el-form-item label="Subjects taught:" label-width="180px">
             <el-input v-model="form.subject"></el-input>
           </el-form-item>
-          <el-form-item label="Professional ranks:">
+          <el-form-item label="Professional ranks:" label-width="180px">
             <el-input v-model="form.profession"></el-input>
           </el-form-item>
           <el-form-item>
@@ -50,6 +60,9 @@ export default {
         profession: '',
         desc: '',
       },
+      name: "Ann",
+      post: "Head teacher",
+      classtaught: "Class 11, Class 12",
     }
   },
   methods: {
@@ -77,31 +90,56 @@ export default {
 .home-container {
   height: 100%;
 }
+
 .right {
   width: 1303.2px;
   float: left;
 }
+
 .main {
   width: 100%;
-  overflow-y:auto;
+  overflow-y: auto;
   height: 700px;
 }
-.el-form{
-  width: 1000px;
+
+.title{
+  width: 100%;
+  height: 120px;
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%; //图片变为圆角图片
+    margin-left: 130px;
+    padding: 20px 35px;
+    float: left;
+  }
+  .char {
+    font-size: 20px;
+    margin: 40px 30px;
+    float: left;
+  }
+}
+
+
+.el-form {
+  width: 1100px;
   height: 450px;
   border: 3px solid #98abc7;
   border-radius: 1%;
   margin: 20px 100px 20px 100px;
 }
-.el-form-item{
+
+.el-form-item {
   margin-left: 20px;
   margin-top: 20px;
 }
-.el-input{
+
+.el-input {
   width: 350px;
   height: 50px;
 }
-.el-button{
+
+.el-button {
   border: 2px solid #98abc7;
   border-radius: 2%;
   margin: 20px 30px;
