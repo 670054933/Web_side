77ddfc3dc79474f00630c6773fec69f5c7492600
zxx <670054933@qq.com>
2021-12-15 23:02:21 +0800

    1

--- src/components/Headers.vue
+++ src/components/Headers.vue
@@ -1,24 +1,35 @@
 <template>
   <el-header>
     <el-button
+    icon="el-icon-arrow-left"
+    style="float: left;margin-top: 10px"
+    @click="back"
+    ></el-button>
+    <el-button
         icon="el-icon-switch-button"
         circle
-        style="color: red;"
+        style="color: red;float: right;margin-top: 10px"
     ></el-button>
   </el-header>
 </template>
 
 <script>
 export default {
-  name: "Headers"
+  name: "Headers",
+  methods:{
+    back(){
+      console.log(111)
+      this.$router.go(-1)
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
 .el-header {
   background-color: #c3c2c2;
-  display: flex;
-  justify-content: flex-end;
+  display: inline-block;
   align-items: center;
+  width: 100%;
 }
 </style>
