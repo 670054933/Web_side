7e67e37e020fddcc5edea362da2de238b5211521
QX0920 <92423672+QX0920@users.noreply.github.com>
2021-12-15 20:14:57 +0800

    add communication div

--- src/views/Communicate.vue
+++ src/views/Communicate.vue
@@ -4,7 +4,28 @@
     <div class="right">
       <Header></Header>
       <div>
-        内容写在这里
+        <p>You can send notifications to students or parents.</p>
+      </div>
+      <div>
+        <span>Student:</span>
+        <span>Parent:</span>
+      </div>
+      <div class="class11">
+
+        <div class="studentsCommunication">
+<!--          <img src="@/image/studentCommunication.png" alt="">-->
+        </div>
+        <div class="parentsCommunication">
+<!--          <img src="@/image/parentCommunication.png" alt="">-->
+        </div>
+      </div>
+      <div class="class12">
+        <div class="studentsCommunication">
+<!--          <img src="@/image/studentCommunication.png" alt="">-->
+        </div>
+        <div class="parentsCommunication">
+<!--          <img src="@/image/parentCommunication.png" alt="">-->
+        </div>
       </div>
     </div>
   </div>
@@ -20,8 +41,21 @@ export default {
 </script>
 
 <style lang="less" scoped>
-//.right{
-//  width: 86%;
-//  float: left;
-//}
+.class11{
+  width: 100%;
+  height: 200px;
+}
+.class12{
+  width: 100%;
+  height: 200px;
+}
+.studentsCommunication{
+  width: 200px;
+  height: 100px;
+  //background: rgba(0, 0, 0, 0)
+  //background-image: url("../image/studentCommunication.png") no-repeat scroll 100% 100%;
+  //background-image: url("../image/studentCommunication.png");
+  background-size: auto;
+  background-size: cover;
+}
 </style>
