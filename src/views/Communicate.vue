e18c390b408e226ac0fc88570a974dd55ade977f
QX0920 <92423672+QX0920@users.noreply.github.com>
2021-12-20 18:31:37 +0800

    add communication div

--- src/views/Communicate.vue
+++ src/views/Communicate.vue
@@ -4,8 +4,26 @@
     <div class="right">
       <Headers :isShow="isShow"></Headers>
       <div class="main">
-        <div class="S_List">
-            askdflkjaslkdfjl
+        <div class="notice">
+          <span>You can send notifications to students or parents.</span>
+        </div>
+        <div class="choice">
+          <div class="class_top">
+            <div class="class1">
+              <p class="s">Students in Class 11</p>
+            </div>
+            <div class="class2">
+              <p class="p">Parents in Class 11</p>
+            </div>
+          </div>
+          <div class="class_bottom">
+            <div class="class1">
+              <p class="s">Students in Class 12</p>
+            </div>
+            <div class="class2">
+              <p class="p">Parents in Class 12</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -37,9 +55,57 @@ export default {
   overflow-y:hidden;
   height: 700px;
 }
+.notice{
+  width: 100%;
+  height: 5%;
+  padding-left: 20px;
+  color: #9f9c96;
+  font-size: 24px;
+}
+.choice{
+  width: 100%;
+  height: 90%;
+}
+.class_top{
+  width: 100%;
+  height: 40%;
+}
 
-.S_List{
-  width: 100px;
-  height: 100%;
+.class_bottom{
+  width: 100%;
+  height: 40%;
+}
+
+.class1{
+  float: left;
+  width: 300px;
+  height: 150px;
+  margin-top: 30px;
+  margin-left: 100px;
+  margin-right: 100px;
+  background: rgba(0, 0, 0, 0) url("../image/student.png") no-repeat;
+  background-size: 100%;
+}
+
+.class2{
+  float: left;
+  width: 300px;
+  height: 150px;
+  margin-top: 30px;
+  margin-left: 150px;
+  background: rgba(0, 0, 0, 0) url("../image/parent.png") no-repeat;
+  background-size: 100%;
+}
+.s{
+  color: #6284d0;
+  font-size: 20px;
+  margin-left: 30%;
+  margin-top: 15%;
+}
+.p{
+  color: #9f7037;
+  font-size: 20px;
+  margin-left: 30%;
+  margin-top: 15%;
 }
 </style>
